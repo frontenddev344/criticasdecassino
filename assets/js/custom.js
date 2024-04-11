084f6b4d7a3dd26b80cd3be467167632b85d2a47
devavology <145644105+devavology@users.noreply.github.com>
2024-04-11 18:06:59 +0530

    dafhsfgh

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,3 +14,14 @@ $(window).scroll(function(){
       $('header').removeClass('fixed-header');
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var acceptCookiesBtn = document.getElementById("accept-cookies");
+  var cookiesPopup = document.getElementById("cookies-popup");
+
+  acceptCookiesBtn.addEventListener("click", function() {
+      cookiesPopup.style.display = "none";
+      // You can add code here to set a cookie to remember the user's choice
+  });
+});
